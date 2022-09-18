f6177fc3366ea88a5512e497e9ca508af1077b21
shubham423 <shubh991172@gmail.com>
2022-09-18 14:49:51 +0530

    fixes #33 - add buttons (#34)

---
+++ app/src/main/java/com/example/affirmationtask/ui/components/SecondoryButton.kt
@@ -0,0 +1,57 @@
+package com.example.affirmationtask.ui.components
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.intl.Locale
+import androidx.compose.ui.text.toUpperCase
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.affirmationtask.R
+import com.example.affirmationtask.ui.theme.ButtonShape
+import com.example.affirmationtask.ui.theme.TAOTheme
+
+@Composable
+fun SecondaryButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    TextButton(
+        onClick = onClick,
+        shape = ButtonShape,
+        modifier = modifier
+            .height(dimensionResource(id = R.dimen.button_height))
+            .fillMaxWidth(),
+    ) {
+        Text(
+            text = text.toUpperCase(Locale.current),
+        )
+    }
+}
+
+@Preview(
+    name = "Night Mode",
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+)
+@Preview(
+    name = "Day Mode",
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+)
+@Composable
+@Suppress("UnusedPrivateMember")
+private fun SecondaryButtonPreview() {
+    TAOTheme {
+        Surface {
+            SecondaryButton(
+                text = "Primary button",
+                onClick = {},
+            )
+        }
+    }
+}
