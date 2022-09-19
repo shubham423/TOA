345db032da6d607b74ba8e9b098ace303eebc2dd
shubham423 <shubh991172@gmail.com>
2022-09-19 18:01:44 +0530

    Login screen ui

---
+++ app/src/main/java/com/example/affirmationtask/ui/components/VerticalSpacer.kt
@@ -0,0 +1,16 @@
+package com.example.affirmationtask.ui.components
+
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+
+/**
+ * This is a custom [Spacer] that consumes a [height] and removes boilerplate.
+ */
+@Composable
+fun VerticalSpacer(height: Dp) {
+    Spacer(modifier = Modifier.height(height))
+}
